2eb991863a67888377f61a84a2ae77add7052430
1gilmar <1gilmar@gmail.com>
2021-03-29 18:41:34 -0300

    ajust ident

--- hooks/fetch.ts
+++ hooks/fetch.ts
@@ -14,14 +14,14 @@ import api from "../services/api";
 
 // usando axios
 export const useFetch = <Data = any, Error = any>(url: string) => {
-// export const useFetch = (url: string) => {
-    const { data, error } = useSWR<Data, Error>(url, async url => {
-    // const { data } = useSWR(url, async url => {
-        const response = await api.get(url)
+	// export const useFetch = (url: string) => {
+	const { data, error } = useSWR<Data, Error>(url, async url => {
+		// const { data } = useSWR(url, async url => {
+		const response = await api.get(url)
 
-        return response.data;
-    })
+		return response.data;
+	})
 
-    return { data, error }
-    // return { data }
+	return { data, error }
+	// return { data }
 }
