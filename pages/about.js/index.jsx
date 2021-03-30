bc2c513b0132c1f66bd86e50650d08227938ad6e
1gilmar <1gilmar@gmail.com>
2021-03-30 08:46:03 -0300

    ajustado vercel json para depoi

--- pages/about.js/index.jsx
+++ pages/about.js/index.jsx
@@ -1,10 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+
 function About({ segredos }) {
   return (
     <div>
-      <ul>
-        {segredos}
-      </ul>
+    <Head>
+      <title>About</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    {/* <Nav /> */}
+
+    <div>
+      <h1>Macro Compliance Tracker!</h1>
+      <p>
+        This app will help you ensure your macros are within a selected range to help you achieve your New Years Resolution!
+      </p>
     </div>
+  </div>
 
   )
 }
