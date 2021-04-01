feeb4d2d263fb3441f2d938f295837053dbb27c1
1gilmar <1gilmar@gmail.com>
2021-04-01 11:44:36 -0300

    Criado a pagina final statica

---
+++ pages/segredos/final.tsx
@@ -0,0 +1,32 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next"
+
+export const getStaticProps: GetStaticProps = async () => {
+	type Segredo ={
+		_id: string,
+		segredo: string,
+		cor: string,
+	}
+
+  const res = await fetch('http://localhost:3000/api/segredos')
+  const segredos: Segredo[] = await res.json()
+
+  return {
+    props: {
+      segredos,
+    },
+		revalidate: 1
+  }
+}
+
+// posts will be populated at build time by getStaticProps()
+function Blog({ segredos }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <ul>
+      {segredos.map((segredo) => (
+        <li>{segredo.segredo}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default Blog
