9dfb063373fc475feaee95e77ada74a73c9b0a23
1gilmar <1gilmar@gmail.com>
2021-04-02 12:22:26 -0300

    criado tellme statica

---
+++ pages/tellme/index.tsx
@@ -0,0 +1,116 @@
+import { useAxios } from '@/hooks/axiosinicial';
+import api from '@/services/api';
+import setCor from '@/services/cor';
+import Link from 'next/link'
+import router from 'next/router';
+import React, { useState } from 'react';
+
+interface ISegredo {
+  _id: string,
+  segredo: string,
+  cor: string
+}
+
+const Index = ({initialData}) => {
+  //descontruo a arrai data e dou o nome dele de segredos
+//   const { data: segredos, mutate } = useAxios<ISegredo[]>('/api/segredos', initialData);
+  
+  interface ISegredoState {
+    segredo: string,
+    cor: string,
+    dataAt: any
+  }
+
+  const [model, setModel] = useState<ISegredoState>({
+    segredo: '',
+    cor: '',
+    dataAt: ''
+  })
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const target = event.target
+
+    setModel({
+      ...model,
+      [target.name]: target.value,
+    })
+  }
+
+  const handleSubmit = () => {
+    model.dataAt = Date.now()
+    model.cor = setCor()
+
+    api.post('/api/segredos', model).then(res => {
+      console.log('salvou...', Date.now())
+    }).catch(error => {
+      console.log('erro ao salvar registro: ', error)
+    })
+
+    model.segredo = ''
+    model.cor = ''
+    model.dataAt = ''
+
+    router.push('/')
+
+    // mutate()
+
+  }
+  return (
+    <>
+      <div className="container">
+        <div className="col s12 m3"></div>
+        <div className="col s12 m6">
+          <div className="card  blue-grey darken-2">
+            <div className="card-content white-text">
+              <span className="card-title"></span>
+
+              <textarea value={model.segredo} onChange={handleChange}
+                name="segredo" id="segredo" placeholder="you need help..."
+                className="materialize-textarea white-text" required >
+              </textarea>
+              <button onClick={handleSubmit}
+                className="btn waves-effect waves-light right pulse" >tellme
+							</button>
+
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m3"></div>
+      </div>
+
+
+      <div className="container">
+        {initialData?.map((item) => (
+          <div className="card" key={item._id}>
+            <div className="col s12 m3"></div>
+            <div className="col s12 m6">
+
+              <div className={`card ${item.cor}`}>
+                <div className={"card-content white-text"}>
+                  <span className="card-title"></span>
+                  <p >{item.segredo} </p>
+                </div>
+                <div className="card-action" >
+                  <Link href="/" >good </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m3"></div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Index
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.HOST}/api/segredos`)
+
+  const segredos = await res.json()
+
+  return { props: { initialData: segredos } }
+}
